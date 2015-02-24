d9353d998de8ddc1d61c26975ddaae76d9c19d7f
justintin <justintin1987@gmail.com>
2015-02-24 15:36:04 -0500

    detailed job page finished

--- modules/organization_manager/theme/zd-find-jobs-detailed-job-page.tpl.php
+++ modules/organization_manager/theme/zd-find-jobs-detailed-job-page.tpl.php
@@ -1,4 +1,4 @@
-<div id="zd-job-page-<?php print $job->id; ?>" class="zd-job-page-wrapper">
+<div id="zd-job-page-<?php print $job->ID; ?>" class="zd-job-page-wrapper">
     <div class="container-fluid">
         <div class="row-fluid job-top-block">
             <!-- <div class="span2 text-left job-logo">
@@ -6,8 +6,8 @@
             </div> -->
             <div class="span12">
                 <div class="row-fluid">
-                    <div class="span8 text-left job-title">
-                        <?php print $job->title; ?>
+                    <div class="span8 text-left job-Title">
+                        <?php print $job->Title; ?>
                     </div>
                     <div class="span4 text-right job-price">
 
@@ -19,27 +19,23 @@
                         <div class="company-location"><?php print $job->location; ?></div>
                     </div>
                     <div class="span4 text-right job-btns share-view">
-                        <?php if(!empty($job->url)): ?>
+                        <?php if(!empty($job->Url)): ?>
                             <?php if ($user->uid > 0): ?>
-                                <a href="<?php print $job->url; ?>" target="_blank" class="btn btn-danger btn-apply"><?php print t('Apply'); ?></a>
+                                <a href="<?php print $job->Url; ?>" target="_blank" class="btn btn-danger btn-apply"><?php print t('Apply'); ?></a>
                             <?php else: ?>
                                 <a href="#" onclick="App.singInPopup(); return false;" class="btn btn-danger btn-apply"><?php print t('Apply'); ?></a>
                             <?php endif; ?>
                         <?php endif; ?>
-                        <a href="<?php print $home; ?>/job-details/<?php print $job->id; ?>" 
+                        <a href="<?php print $home; ?>/detailed-job-page/<?php print $job->ID; ?>" 
                             class="btn btn-danger btn-share" 
                             data-img="http://placehold.it/120x90&text=Logo"
-                            data-id="<?php print $job->id; ?>"
+                            data-id="<?php print $job->ID; ?>"
                             data-title="ZoomDojo - cracking the Job Search Code for You"
-                            data-desc="Click here to see a job opportunity <?php print $job->title; ?>">
+                            data-desc="Click here to see a job opportunity <?php print $job->Title; ?>">
                             <?php print t('Share'); ?>
                             <i id="share-btn"></i>
                         </a>
-                        <?php if($job->saved): ?>
-                            <a href="#" class="btn btn-danger btn-save btn-saved" data-id="<?php print $job->id; ?>" data-uid="<?php print $user->uid; ?>"><span class="saved-text-for-replace"><?php print t('Saved'); ?></span> <i id="save-btn"></i> </a>
-                        <?php else: ?>
-                            <a href="#" class="btn btn-danger btn-save tooltips" data-tooltips-text="<?php print t('Save this employer in your account for future reference'); ?>" data-id="<?php print $job->id; ?>" data-uid="<?php print $user->uid; ?>"><span class="saved-text-for-replace"><?php print t('Save'); ?></span> <i id="save-btn"></i> </a>
-                        <?php endif; ?>
+                        
                     </div>
                 </div>
             </div>
@@ -49,10 +45,10 @@
                 <h3 class="job-info-text"><?php print t('Job Details'); ?></h3>
             </div>
             <div class="span2 job-status text-right">
-                <div class="posted">Posted <?php print $job->date; ?> </div>
+                <div class="posted">Posted <?php print $job->Created_on; ?> </div>
             </div>
             <div class="span12 job-info">
-                <div class="job-description"><?php print $job->description; ?></div>
+                <div class="job-description"><?php print $job->Snippet; ?></div>
             </div>
         </div>
     </div>
