6e8706f7a9a82f1a7a1d99338e3b46d42b76c0a1
Zheng-Li <zl714@nyu.edu>
2015-02-25 16:28:50 -0500

    ready for pull

--- modules/organization_manager/theme/zd-find-jobs-header-form.tpl.php
+++ modules/organization_manager/theme/zd-find-jobs-header-form.tpl.php
@@ -4,7 +4,7 @@
         <form id="findJobsFormMini" action="/find-positions" method="get" >
             <div class="row-fluid" style="position:relative; right:90px;">
                 <input name="keywords" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type any keywords" class="input-large span5 keywords-autocomplete" style="margin-left:120px;"/>
-                <input name="location" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type location" class="input-large span5 location-autocomplete" />
+                <input name="location" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type location" class="input-large span5 location-autocomplete" style="margin-right:23px;"/>
             </div>
         </form>
         <a class="btn btn-danger btn-small fr-search-btn" style="position:relative; right:90px;" href="#" onclick="jQuery('#findJobsFormMini').submit(); return false;">Search</a>
