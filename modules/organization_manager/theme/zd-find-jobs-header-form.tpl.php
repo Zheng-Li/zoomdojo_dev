e68c5a4f61301b97f6cdcd86301a81d01d805cf4
Zheng-Li <zl714@nyu.edu>
2015-02-26 16:35:48 -0500

    x

--- modules/organization_manager/theme/zd-find-jobs-header-form.tpl.php
+++ modules/organization_manager/theme/zd-find-jobs-header-form.tpl.php
@@ -1,12 +1,12 @@
 <div class="front-top-block">
-    <div class="front-tb-left" style="position:relative; left:30px;">Search Jobs & Internships</div>
+    <div class="front-tb-left" style="position:relative;">Search Jobs & Internships</div>
     <div class="front-tb-right">
         <form id="findJobsFormMini" action="/find-positions" method="get" >
             <div class="row-fluid" style="position:relative; right:90px;">
-                <input name="keywords" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type any keywords" class="input-large span5 keywords-autocomplete" style="margin-left:120px;"/>
-                <input name="location" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type location" class="input-large span5 location-autocomplete" style="margin-right:23px;"/>
+                <input name="keywords" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type any keywords" class="input-large span5 keywords-autocomplete" style="margin-left:108px;"/>
+                <input name="location" value="" id="employer-autocomplete" type="text" autocomplete="off" placeholder="Type location" class="input-large span5 location-autocomplete" style="margin-left: 35px;"/>
             </div>
         </form>
-        <a class="btn btn-danger btn-small fr-search-btn" style="position:relative; right:90px;" href="#" onclick="jQuery('#findJobsFormMini').submit(); return false;">Search</a>
+        <a class="btn btn-danger btn-small fr-search-btn" style="position:relative; right:20px;" href="#" onclick="jQuery('#findJobsFormMini').submit(); return false;">Search</a>
     </div>
 </div>
